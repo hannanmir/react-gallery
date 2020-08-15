bc63f810df24f1604bd82da30a7dcfa06b11899f
Hannan Mir <hannanmir2@gmail.com>
2020-08-15 00:42:09 -0500

    database up and running

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+// const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
-}); // END PUT Route
+    console.log("In PUT", req.params.id);
+    let queryText = `
+        UPDATE "images"
+        SET "likes" = "likes" + 1
+        WHERE "id" = $1;
+        `;
+    pool.query(queryText, [req.params.id])
+        .then((result) => {
+        res.sendStatus(200);
+    })
+    .catch((error) => {
+        console.log("error in likeImage", error);
+        res.sendStatus(500);
+    });
+});
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
-}); // END GET Route
+    let queryText = 'SELECT * FROM "images" ORDER BY "id";';
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log('error getting images', error);
+        res.sendStatus(500);
+    });
+})
 
 module.exports = router;
